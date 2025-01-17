048331c5204e7fd5cb06620e2786d34f0ae9db52
Gunbacca <albe07001@utb.vaxjo.se>
2025-01-17 13:54:41 +0100

    ff
    ff

--- Program.cs
+++ Program.cs
@@ -1,15 +1,38 @@
-﻿List<int>tal= new List<int>();
+﻿using System.Diagnostics;
+/*
+List<int>tal= new List<int>();
 tal.Add(1);
 tal.Add(3);
 tal.Add(2);
-tal.Add(7);
-tal.Add(5);
-for(int i=0; i<tal.Count; i++){
-    for(int j=0; j<tal.Count-1-i; j++){
-        if(tal[j]>tal[j+1]){
-            int swap= j;
-            j=i;
-            i=swap;
+tal.Add(2);
+tal.Add(53);
+tal.Add(95);
+tal.Add(12);
+tal.Add(88);
+tal.Add(23);
+tal.Add(44);
+*/
+List<int> tal=Enumerable.Range(1,1000).ToList();
+
+
+ Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        BubbleSort(tal);
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"Tiden för loopen att köra: {stopwatch.Elapsed.Microseconds} μs");
+    
+
+void BubbleSort(List<int>bubble){
+    for(int i=0; i<bubble.Count; i++){
+    for(int j=0; j<bubble.Count-1-i; j++){
+        if(bubble[j]>bubble[j+1]){
+            int swap= bubble[j];
+            bubble[j]=bubble[j+1];
+            bubble[j+1]=swap;
         }
     }
 }
+} 
--- obj/Debug/net8.0/sotering.AssemblyInfo.cs
+++ obj/Debug/net8.0/sotering.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sotering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a14a171ac60ed8e6d4e360260567e1f534f344")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7585de9107876b05c203e831fa2eed950cb6b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("sotering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sotering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
