43695d1a6fc165371f8040cebbd117a0f0dda772
Gunbacca <albe07001@utb.vaxjo.se>
2025-01-19 12:34:04 +0100

    ff
    ff

--- Program.cs
+++ Program.cs
@@ -1,19 +1,8 @@
 ﻿using System.Diagnostics;
-/*
-List<int>tal= new List<int>();
-tal.Add(1);
-tal.Add(3);
-tal.Add(2);
-tal.Add(2);
-tal.Add(53);
-tal.Add(95);
-tal.Add(12);
-tal.Add(88);
-tal.Add(23);
-tal.Add(44);
-*/
-List<int> tal=Enumerable.Range(1,1000).ToList();
 
+List<int> tal = Enumerable.Range(1, 100000).ToList();
+Random random = new Random();
+tal = tal.OrderBy(x => random.Next()).ToList();
 
  Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
@@ -36,3 +25,107 @@ void BubbleSort(List<int>bubble){
     }
 }
 } 
+/*
+using System.Diagnostics;
+
+List<int> tal = Enumerable.Range(1, 10000).ToList();
+Random random = new Random();
+tal = tal.OrderBy(x => random.Next()).ToList();
+
+Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+       SelectionSort(tal);
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"Tiden för loopen att köra: {stopwatch.Elapsed.Microseconds} μs");
+    
+
+void SelectionSort(List<int> list)
+{
+    int n = list.Count;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (list[j] < list[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        int temp = list[minIndex];
+        list[minIndex] = list[i];
+        list[i] = temp;
+    }
+}
+*/
+ /*void MergeSort(List<int> list)
+{
+    if (list.Count <= 1)
+        return;
+
+    int mid = list.Count / 2;
+    List<int> left = list.GetRange(0, mid);
+    List<int> right = list.GetRange(mid, list.Count - mid);
+
+    MergeSort(left);
+    MergeSort(right);
+    Merge(list, left, right);
+}
+
+void Merge(List<int> list, List<int> left, List<int> right)
+{
+    int i = 0, j = 0, k = 0;
+    while (i < left.Count && j < right.Count)
+    {
+        if (left[i] <= right[j])
+        {
+            list[k++] = left[i++];
+        }
+        else
+        {
+            list[k++] = right[j++];
+        }
+    }
+    while (i < left.Count)
+    {
+        list[k++] = left[i++];
+    }
+    while (j < right.Count)
+    {
+        list[k++] = right[j++];
+    }
+}
+*/
+ /*void QuickSort(List<int> list, int low, int high)
+{
+    if (low < high)
+    {
+        int pi = Partition(list, low, high);
+        QuickSort(list, low, pi - 1);
+        QuickSort(list, pi + 1, high);
+    }
+}
+
+ int Partition(List<int> list, int low, int high)
+{
+    int pivot = list[high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++)
+    {
+        if (list[j] < pivot)
+        {
+            i++;
+            int temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
+    }
+    int swapTemp = list[i + 1];
+    list[i + 1] = list[high];
+    list[high] = swapTemp;
+    return i + 1;
+}
+*/
--- obj/Debug/net8.0/sotering.AssemblyInfo.cs
+++ obj/Debug/net8.0/sotering.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sotering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7585de9107876b05c203e831fa2eed950cb6b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+048331c5204e7fd5cb06620e2786d34f0ae9db52")]
 [assembly: System.Reflection.AssemblyProductAttribute("sotering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sotering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
