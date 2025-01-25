fd9ba18648f731146eeb33b30a2116cf8b58dc86
Gunbacca <albe07001@utb.vaxjo.se>
2025-01-25 15:37:19 +0100

    ff
    ff

--- obj/Debug/net8.0/sotering.AssemblyInfo.cs
+++ obj/Debug/net8.0/sotering.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sotering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b90d71caf73670001bb19afed1de34d33979e545")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa66dd81b795ce6fadac2fd56049fa44d7316112")]
 [assembly: System.Reflection.AssemblyProductAttribute("sotering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sotering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
